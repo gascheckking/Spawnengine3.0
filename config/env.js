6ddcd86cb56727f02301e8c4c3376ea7550f74fc
gascheckking <warpaiactivity1@gmail.com>
2025-12-23 21:17:17 +0100

    Delete env.js

--- config/env.js
+++
@@ -1,8 +0,0 @@
-export const ENV = {
-  MODE: "production",
-  API_BASE: "https://api.spawnengine.io/v1",
-  BASE_CHAIN: "base-mainnet",
-  ZORA_CHAIN: "zora-mainnet",
-  FARCASTER_API: "https://api.farcaster.xyz",
-  DEBUG: false,
-};
